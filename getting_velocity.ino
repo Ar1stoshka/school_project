262f77685951fe6eccf3d3827c7a9875995cfd5b
Ar1stoshka <126413479+Ar1stoshka@users.noreply.github.com>
2024-04-19 23:25:59 +0300

    Update getting_velocity.ino

--- getting_velocity.ino
+++ getting_velocity.ino
@@ -1,39 +1,40 @@
-float V_0 = 5.0;
-float rho = 1.204;
-int ledPin = 3; 
+float V_0 = 5.0;  // напряжение питания датчика
+float rho = 1.204;  // плотность воздуха при 20 градусах
+int ledPin = 3;   // пин в который вставлен светодиод
 
+// параметры задающие усреднение и смещение
 int offset = 0;
 int offset_size = 10;
 int veloc_mean_size = 20;
 int zero_span = 2;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);  // начало обмена данными с датчиком
   pinMode(5, OUTPUT);
   pinMode(ledPin, OUTPUT);  
-  for (int ii = 0; ii < offset_size; ii++){
-    offset += analogRead(A0) - (1023 / 2);
+  for (int ii = 0; ii < offset_size; ii++) {
+    offset += analogRead(A0) - (1023 / 2);  // считываем данные с датчика и учитывам погрешность
   }
   offset /= offset_size;
 }
 
 
 void loop() {
-  float adc_avg = 0; 
-  float veloc = 0.0;
-  for (int ii = 0; ii < veloc_mean_size; ii++){
+  float adc_avg = 0;  // усреднение показателей АЦП
+  float veloc = 0.0;  // скорость
+  for (int ii = 0; ii < veloc_mean_size; ii++) {
     adc_avg += analogRead(A0) - offset;
   }
-  adc_avg /= veloc_mean_size;
-  if (adc_avg > 512 - zero_span and adc_avg < 512 + zero_span){
-  } else{
-    if (adc_avg < 512){
+  adc_avg /= veloc_mean_size;  // усреднение показателей АЦП
+  if (adc_avg > 512 - zero_span and adc_avg < 512 + zero_span) {  // если показания АЦП меньше 512 -> скорость отрицательная 
+  } else {
+    if (adc_avg < 512) {
       veloc = -sqrt((-10000.0 * ((adc_avg / 1023.0) - 0.5)) / rho);
     } else{
-      veloc = sqrt((10000.0  *((adc_avg / 1023.0) - 0.5)) / rho);
+      veloc = sqrt((10000.0 * ((adc_avg / 1023.0) - 0.5)) / rho);
     }
   }
-  if (veloc > 0.00) {
+  if (veloc > 0.00) {  // если скоростьбольше нуля -> все в норме, сигнализация отсутствует  
     digitalWrite(ledPin, LOW);
     noTone(5);
   } else {  //  если скорость падает
